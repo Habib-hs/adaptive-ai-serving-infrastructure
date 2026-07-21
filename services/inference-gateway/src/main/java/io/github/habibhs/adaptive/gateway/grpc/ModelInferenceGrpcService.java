e35ebfc5dfa9dcc08b94cc8e4614e6418bec0e4d
Habib-hs <mhabibullahhs@gmail.com>
2026-07-21 15:21:42 +0600

    feat(gateway): add gRPC service implementation skeleton

---
+++ services/inference-gateway/src/main/java/io/github/habibhs/adaptive/gateway/grpc/ModelInferenceGrpcService.java
@@ -0,0 +1,7 @@
+package io.github.habibhs.adaptive.gateway.grpc;
+
+import io.github.habibhs.adaptive.contracts.inference.v1.ModelInferenceServiceGrpc;
+
+public class ModelInferenceGrpcService extends ModelInferenceServiceGrpc.ModelInferenceServiceImplBase {
+
+}
